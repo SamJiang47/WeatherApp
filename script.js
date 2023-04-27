74660d2749a131be5510799b853743bb570d7de6
Sam Jiang <129788706+SamJiang47@users.noreply.github.com>
2023-04-26 22:31:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let latitude=34.052235;
-let longitude=-118.243683;
+let latitude=0;
+let longitude=0;
 window.onload = function() {
                             const date = new Date();
                             const dateString = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
@@ -9,6 +9,15 @@ if ("geolocation" in navigator) {
                                 } else {
                                         console.log("Geolocation is not available in your browser.");
                                        }
-
-    
+function success(position){
+                            latitude = position.coords.latitude;
+                            longitude = position.coords.longitude;
+                          }
+btn.addEventListener("click", () => {
+                                     let forecast = [["M", 52], ["Tu", 53], ["W", 54], ["Th", 55], ["F", 56]]
+                                     let forecastElements = document.getElementsByClassName("forecast");
+                                     for (let i = 0; i < forecast.length; i++) {
+                                                                                forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "°F";
+                                                                               }
+                                    });
     
