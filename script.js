a4b2936a5d2e286217b3362ea73886aeebb85853
Sam Jiang <129788706+SamJiang47@users.noreply.github.com>
2023-04-26 22:27:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ window.onload = function() {
                             const date = new Date();
                             const dateString = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
                            }
+if ("geolocation" in navigator) {
+                                 navigator.geolocation.getCurrentPosition(success)
+                                } else {
+                                        console.log("Geolocation is not available in your browser.");
+                                       }
+
     
     
