aca47a1bd247e2c2d921d7aac6a5c7a96040cb96
Sam Jiang <129788706+SamJiang47@users.noreply.github.com>
2023-04-26 22:23:19 -0700

    Add jvscript

---
+++ script.js
@@ -0,0 +1,8 @@
+let latitude=34.052235;
+let longitude=-118.243683;
+window.onload = function() {
+                            const date = new Date();
+                            const dateString = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
+                           }
+    
+    
