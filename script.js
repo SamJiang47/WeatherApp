88cfd4a541dca722de123ef47da26e9d6015e6f3
SamJiang47 <zjiang89@ucsc.edu>
2023-05-01 17:42:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var latitude
 var longitude
-const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];//
 
 window.onload = function() {
                             const date = new Date();
@@ -20,35 +20,10 @@ window.onload = function() {
                             
 	                          const btn = document.getElementById("getWeatherBtn")
 	                          btn.addEventListener("click", () => {
-	                          const lat = latitude;
-	                          const lon = longitude;
-	                          const xhr = new XMLHttpRequest();
-	                          xhr.open("GET", `http://localhost:3000/weather/${lat}/${lon}`);
-	                          xhr.send();
-
-	                          xhr.onload = () => {
-                                                const response = JSON.parse(xhr.responseText);
-		                                            console.log(response)
-                                                const temperature = response.temperature;
-                                                const weatherStatus = response.weatherStatus;
-
-                                                document.getElementById("temperature").innerHTML = `Temperature: ${temperature}°F`;
-                                                document.getElementById("weatherStatus").innerHTML = `Weather Status: ${weatherStatus}`;
-	                                             };
-	                          const xhr2 = new XMLHttpRequest();
-	                          xhr2.open("GET", `http://localhost:3000/weather/forecast/${lat}/${lon}`);
-	                          xhr2.send();
-	                          xhr2.onload = function () {
-	                                                     if (xhr2.status === 200) {
-		                                                                             var response = JSON.parse(xhr2.responseText);
-		                                                                             var forecast = response.forecast;
-		                                                                             var forecastElements = document.getElementsByClassName("forecast");
-		                                                                             for (var i = 0; i < forecast.length; i++) {
-			                                                                                                                      forecastElements[i].innerHTML = forecast[i].dayName + ": " + forecast[i].temp + "°F";
-                                                                                                                           }
-                                                                                } else {
-                                                                                        console.error(xhr2.responseText);
-                                                                                       }
-                                                      }
-                                                });
-                                              }
+                                                                 let forecast = [["M", 52], ["Tu", 53], ["W", 54], ["Th", 55], ["F", 56]]
+                                                                 let forecastElements = document.getElementsByClassName("forecast");
+                                                                 for (let i = 0; i < forecast.length; i++) {
+                                                                                                            forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "°F";
+                                                                                                           }
+                                                                });
+                          }
