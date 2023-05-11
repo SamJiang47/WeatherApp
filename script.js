d2e54b56fcd1a64fb46c1c7703954dee7a22018d
SamJiang47 <zjiang89@ucsc.edu>
2023-05-10 21:43:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -23,10 +23,46 @@ function success(position){
                             
 	                          const btn = document.getElementById("getWeatherBtn")
 	                          btn.addEventListener("click", () => {
+
+                                                                     const xhr = new XMLHttpRequest();
+                                                                     xhr.open("GET", `http://localhost:3000/weather/${lat}/${lon}`); 
+                                                                     xhr.send();
+                                                                     xhr.onload = function() {                 
+                                                                                              const body = JSON.parse(xhr.responseText);
+                                                                                              let temperature = body.temperature;
+	                                                                                          let weatherStatus = body.weatherStatus;
+                                                                                              document.getElementById("temperature").innerHTML = `Temperature: ${temperature}°F`;
+                                                                                              document.getElementById("weatherStatus").innerHTML = `Weather Status: ${weatherStatus}`;
+
+                                                                                             }
+
+                                                                     var location = "Santa Cruz";
+                                                                     var temp = 59;
+                                                                     var helloString = `Hello, in ${location} it is ${temp} Degrees`;
+                                                                     console.log(helloString);                
+
+
                                                                     var forecast = [["M", 52], ["Tu", 53], ["W", 54], ["Th", 55], ["F", 56]]
                                                                     var forecastElements = document.getElementsByClassName("forecast");
                                                                     for (var i = 0; i < forecast.length; i++) {
                                                                                                                forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "°F";
                                                                                                               }
+
+                                                                    const xhr2 = new XMLHttpRequest();
+                                                                    xhr2.open("GET", `http://localhost:3000/weather/${lat}/${lon}`); 
+                                                                    xhr2.send();
+                                                                    xhr2.onload = function() {                 
+                                                                                              const body = JSON.parse(xhr.responseText);
+                                                                                              var forecast = body.forecast;
+                                                                                              var forecastElements = document.getElementsByClassName("forecast");
+	                                                                                          for (var i = 0; i < forecast.length; i++) {
+                                                                                                                                         forecastElements[i].innerHTML = `${forecast[i].dayName}: ${forecast[i].temp}°F`;           
+                                                                                                                                        }
+		
+                                                                                                    
+
+                                                                  }
+                                                                                                            
+                                                                                                              
                                                                 });
                           
