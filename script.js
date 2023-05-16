b2348833784793839c10226d49f313d2ba641848
SamJiang47 <zjiang89@ucsc.edu>
2023-05-15 18:32:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ let longitude= 0;
 window.onload = function() {
                             const date = new Date();
                             const dateString = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
-                            document.getElementById('date').innerHTML= dateString
+                            document.getElementById('date').innerHTML= dateString;
                            
                             if ("geolocation" in navigator) {
-                                                                navigator.geolocation.getCurrentPosition(success)
+                                                                navigator.geolocation.getCurrentPosition(success);
                                                             } else {
                                                                     console.log("Geolocation is not available in your browser.");
                                                                    }
@@ -21,36 +21,35 @@ function success(position){
                             
 
                             
-	                          const btn = document.getElementById("getWeatherBtn")
+	                          const btn = document.getElementById("getWeatherBtn");
 	                          btn.addEventListener("click", function() {
 
-                                                                     const xhr = new XMLHttpRequest();
-                                                                     xhr.open("GET", `http://localhost:3000/weather/${latitude}/${longitude}`); 
-                                                                     xhr.send();
-                                                                     xhr.onload = function() {                 
-                                                                                              const body = JSON.parse(xhr.responseText);
-                                                                                              let temperature = body.temperature;
-	                                                                                            let weatherStatus = body.weatherStatus;
-                                                                                              document.getElementById("temperature").innerHTML = `Temperature: ${temperature}°F`;
-                                                                                              document.getElementById("weatherStatus").innerHTML = `Weather Status: ${weatherStatus}`;
+                                                                      const xhr = new XMLHttpRequest();
+                                                                      xhr.open("GET", `http://localhost:3000/weather/${latitude}/${longitude}`); 
+                                                                      xhr.send();
+                                                                      xhr.onload = function() {                 
+                                                                                               const body = JSON.parse(xhr.responseText);
+                                                                                               let temperature = body.temperature;
+	                                                                                             let weatherStatus = body.weatherStatus;
+                                                                                               document.getElementById("temperature").innerHTML = `Temperature: ${temperature}°F`;
+                                                                                               document.getElementById("weatherStatus").innerHTML = `Weather Status: ${weatherStatus}`;
 
-                                                                                             }
+                                                                                              }
 
-                                                                    const xhr2 = new XMLHttpRequest();
-                                                                    xhr2.open("GET", `http://localhost:3000/weather/${latitude}/${longitude}`); 
-                                                                    xhr2.send();
-                                                                    xhr2.onload = function() {                 
-                                                                                              const body = JSON.parse(xhr2.responseText);
-                                                                                              var forecast = body;
-                                                                                              var forecastElements = document.getElementsByClassName("forecast");
-	                                                                                            for (var i = 0; i < forecast.length; i++) {
-                                                                                                                                         forecastElements[i].innerHTML = `${forecast[i].dayName}: ${forecast[i].temp}°F`;           
-                                                                                                                                        }
-		
-                                                                                                    
+                                                                      const xhr2 = new XMLHttpRequest();
+                                                                      xhr2.open("GET", `http://localhost:3000/weather/${latitude}/${longitude}`); 
+                                                                      xhr2.send();
+                                                                      xhr2.onload = function() {                 
+                                                                                                const body = JSON.parse(xhr2.responseText);
+                                                                                                var forecast = body;
+                                                                                                var forecastElements = document.getElementsByClassName("forecast");
+	                                                                                              for (var i = 0; i < forecast.length; i++) {
+                                                                                                                                           forecastElements[i].innerHTML = `${forecast[i].dayName}: ${forecast[i].temp}°F`;   
+                                                                                                                                          }      
+                                                                                               }                                                      
 
-                                                                  }
+                                                                                               
                                                                                                             
                                                                                                               
-                                                                });
+                                                                      });
                           
